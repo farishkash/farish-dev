7cb624c99289917c4477ad3219103ab3d450a78a
Farish Kashefinejad <5844161+farishkash@users.noreply.github.com>
2026-08-22 18:56:16 -0700

    Enable MDX for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://farish.dev',
   trailingSlash: 'never',
+  integrations: [mdx()],
 });
