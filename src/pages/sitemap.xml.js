8ad20844dea0cbefce14e6a8708aaeb493c6662f
Farish Kashefinejad <5844161+farishkash@users.noreply.github.com>
2026-08-23 08:28:00 -0700

    Generate sitemap from current blog content

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,8 +1,27 @@
-const pages = ['/', '/work', '/about', '/writing', '/writing/the-starting-point'];
+import { getCollection } from 'astro:content';
+import { BLOG_CATEGORIES } from '../lib/categories';
+import { postUrl, tagSlug } from '../lib/blog';
 
-export function GET() {
-  const urls = pages.map((path) => `<url><loc>https://farish.dev${path}</loc></url>`).join('');
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, {
-    headers: { 'Content-Type': 'application/xml' },
-  });
+export async function GET() {
+  const posts = await getCollection('blog', ({ data }) => !data.draft);
+  const tags = [...new Set(posts.flatMap((post) => post.data.tags))];
+
+  const paths = [
+    '/',
+    '/work',
+    '/about',
+    '/writing',
+    ...posts.map((post) => postUrl(post)),
+    ...BLOG_CATEGORIES.map((category) => `/writing/categories/${category.slug}`),
+    ...tags.map((tag) => `/writing/tags/${tagSlug(tag)}`),
+  ];
+
+  const urls = [...new Set(paths)]
+    .map((path) => `<url><loc>https://farish.dev${path}</loc></url>`)
+    .join('');
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`,
+    { headers: { 'Content-Type': 'application/xml' } },
+  );
 }
